99551f2a6da3215bc93c9926be286bd8076332dc
Alan Zanatta <alan.zanatta@epitech.eu>
2015-01-12 23:48:31 +0100

    first test

---
+++ tek2/piscine/d06/tests.js
@@ -0,0 +1,32 @@
+var testCase = require('nodeunit').testCase;
+var exec = require('child_process').exec;
+var fs = require('fs');
+var sys = require('sys');
+
+module.exports = testCase({
+    'ex00': testCase({
+	'test0': function(test) {
+	    exec("make -C ./ex00/", function(err, stdout, stderr) {
+		if (stderr.length != 0)
+		{
+		    sys.print(stderr);
+		    test.ok(false);
+		    test.done();
+		}
+		else
+		{
+		    exec("./ex00/my_cat inputs/in11 inputs/in12", function(err, stdout, stderr) {
+			fs.readFile("./ex00/outputs/out1", function(err, data) {
+			    test.equal(stdout, data);
+			    test.done();
+			});
+		    })
+		}
+	    });
+	},
+	'test1': function(test) {
+	    
+	    test.done();
+	}
+    })
+});
