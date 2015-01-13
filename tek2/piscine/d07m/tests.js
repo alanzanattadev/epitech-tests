dd3482d0564791638340f7b13dc5461a358d1a85
Alan Zanatta <alan.zanatta@epitech.eu>
2015-01-13 12:41:46 +0100

    untrackked file trackked

---
+++ tek2/piscine/d07m/tests.js
@@ -0,0 +1,38 @@
+var testCase = require('nodeunit').testCase;
+var exec = require('child_process').exec;
+var colors = require('colors');
+var diff = require('diff');
+var fs = require('fs');
+
+module.exports = testCase({
+    'ex00': testCase({
+	'test': function(test) {
+	    exec("/tmp/epitech-tests/test00 | cat -e", function(err, stdout, stderr) {
+		if (err)
+		    {
+			test.ok(false);
+			test.done();
+		    }
+		fs.readFile("/tmp/epitech-tests/ex00/outputs/out1", function(err, data) {
+		    if (err)
+			test.ok(false);
+		    else
+			{
+			    if (data.toString() != stdout)
+				{
+				    var d = diff.diffLines(stdout, data.toString());
+				    d.forEach(function(part){
+					var color = part.added ? 'green' :
+					    part.removed ? 'red' : 'grey';
+					process.stderr.write(part.value[color]);
+				    });
+				    console.log(diff.diffLines(stdout, data.toString()));
+				}
+			    test.equal(stdout, data.toString())
+			}
+		    test.done();
+		});
+	    });
+	}
+    })
+});
