db1353058fe9768658a5463367469a36fff4f568
Alan Zanatta <alan.zanatta@epitech.eu>
2015-01-16 11:02:33 +0100

    Maxime added

--- tek2/piscine/d10/tests.js
+++ tek2/piscine/d10/tests.js
@@ -1,6 +1,8 @@
 var testCase = require('nodeunit').testCase;
 var tests = require('../../../tests.js');
 
+console.log("\x1b[36mMaxime Fischer hérite de Peon qui hérite lui meme de Victim\x1b[0m");
+
 module.exports = testCase({
     'ex00': testCase(tests.getTestCase("test",		"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1"))
 });
