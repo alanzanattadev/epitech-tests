dc20b6067888edd6b23a269650eebf608648f1f8
Kern Matthieu <kern_m@epitech.eu>
2015-01-17 04:47:25 +0100

    updated te

--- tests.js
+++ tests.js
@@ -5,50 +5,48 @@ var diff = require('diff');
 var fs = require('fs');
 
 Error.prepareStackTrace = function(error, stack) {
-    var d = error.message;
-    var ret = "";
-    if (d) {
-	d.forEach(function(part) {
-            var color = part.added ? 'green' : part.removed ? 'red' : 'grey';
-	    ret += part.value[color];
-	});
-    }
-    return ret;
+  var d = error.message;
+  var ret = "";
+  if (d) {
+    d.forEach(function(part) {
+      var color = part.added ? 'green' : part.removed ? 'red' : 'grey';
+      ret += part.value[color];
+    });
+  }
+  return ret;
 };
 
 exports.getTestCase = function(name, execCmd, diffFile) {
-    return ({
-	name: function(test) {
-            exec(execCmd, function(err, stdout, stderr) {
-                if (err) {
-                    test.ok(false);
-		    console.log(stdout);
-		    console.log(stderr);
-		    console.log(err);
-                    test.done();
-                } else {
-                    fs.readFile(diffFile, function(err, data) {
-                        if (err) {
-                            test.ok(false);
-			    console.log(stdout);
-			    console.log(stderr);
-			    console.log(err);
-                        } else {
-                            if (data.toString() != stdout) {
-				var d;
-				if (stdout != "")
-                                    d = diff.diffLines(stdout, data.toString());
-				else
-				    d = diff.diffLines("Your binany doesn't compile ! Check compilation stack below for more informations.\n", stdout);
-				test.ok(false, d);
-                            } else {
-				test.ok(true);
-			    }
-                        }
-                        test.done();
-                    });
-                }
-            });
+  return ({
+    name: function(test) {
+      exec(execCmd, function(err, stdout, stderr) {
+        if (err) {
+          test.ok(false);
+          console.log(stdout);
+          console.log(stderr);
+          console.log(err);
+          test.done();
+        } else {
+          fs.readFile(diffFile, function(err, data) {
+            if (err) {
+              test.ok(false);
+              console.log(stdout);
+              console.log(stderr);
+              console.log(err);
+            } else {
+              if (data.toString() != stdout) {
+                var d;
+                if (stdout != "")
+                  d = diff.diffLines(stdout, data.toString());
+                test.ok(false, d);
+              } else {
+                test.ok(true);
+              }
+            }
+            test.done();
+          });
         }
-    });
+      });
+    }
+  });
 }
