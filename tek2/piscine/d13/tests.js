6d16f5f30ffc1c778a7e8776c91985d129e0230a
Victor Schubert <victor.schubert@epitech.eu>
2015-01-19 10:37:42 +0100

    Add exercise d13e01.

--- tek2/piscine/d13/tests.js
+++ tek2/piscine/d13/tests.js
@@ -4,5 +4,6 @@ var tests = require('../../../tests.js');
 console.log("\x1b[36mWe <3 Zizi-Superstar !\x1b[0m");
 
 module.exports = testCase({
-    'ex00': testCase(tests.getTestCase("Encapsulation",		"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
+  'ex00': testCase(tests.getTestCase("Encapsulation",		"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
+  'ex01': testCase(tests.getTestCase("Canonical form",		"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1"))
 });
