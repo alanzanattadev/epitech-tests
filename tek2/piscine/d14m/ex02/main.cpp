0e930a663a2970abca371ff314dd0b2579ff9693
Alan Zanatta <alan.zanatta@epitech.eu>
2015-01-20 20:19:47 +0100

    test added on ex02 d14m

--- tek2/piscine/d14m/ex02/main.cpp
+++ tek2/piscine/d14m/ex02/main.cpp
@@ -5,32 +5,61 @@
 
 int	main()
 {
-  Coconut**		pack = new Coconut*[14];
-  FruitBox * const *	boxes;
-  int			i = -1;
+  {
+    Coconut**		pack = new Coconut*[14];
+    FruitBox * const *	boxes;
+    int			i = -1;
 
-  boxes = LittleHand::organizeCoconut(NULL);
-  std::cout << boxes << std::endl;
+    boxes = LittleHand::organizeCoconut(NULL);
+    std::cout << boxes << std::endl;
 
-  while (++i < 13)
-    pack[i] = new Coconut;
-  pack[i] = NULL;
-  boxes = LittleHand::organizeCoconut(pack);
-  delete [] pack;
-  i = -1;
-  while (boxes[++i])
+    while (++i < 13)
+      pack[i] = new Coconut;
+    pack[i] = NULL;
+    boxes = LittleHand::organizeCoconut(pack);
+    delete [] pack;
+    i = -1;
+    while (boxes[++i])
+      {
+        std::cout << boxes[i]->nbFruits() << std::endl;
+        delete boxes[i];
+      }
+    delete [] boxes;
+
+    pack = new Coconut*[1];
+    pack[0] = NULL;
+    boxes = LittleHand::organizeCoconut(pack);
+    delete [] pack;
+    std::cout << boxes[0] << std::endl;
+    delete [] boxes;
+  }
+
+  {
+    Coconut**		pack = new Coconut*[19];
+    FruitBox * const *	boxes;
+    int			i = -1;
+
+    boxes = LittleHand::organizeCoconut(NULL);
+    std::cout << boxes << std::endl;
+
+    while (++i < 18)
+      pack[i] = new Coconut;
+    pack[i] = NULL;
+    boxes = LittleHand::organizeCoconut(pack);
+    delete [] pack;
+    i = -1;
+    while (boxes[++i])
     {
       std::cout << boxes[i]->nbFruits() << std::endl;
       delete boxes[i];
     }
-  delete [] boxes;
+    delete [] boxes;
 
-  pack = new Coconut*[1];
-  pack[0] = NULL;
-  boxes = LittleHand::organizeCoconut(pack);
-  delete [] pack;
-  std::cout << boxes[0] << std::endl;
-  delete [] boxes;
-
-  return 0;
+    pack = new Coconut*[1];
+    pack[0] = NULL;
+    boxes = LittleHand::organizeCoconut(pack);
+    delete [] pack;
+    std::cout << boxes[0] << std::endl;
+    delete [] boxes;
+  }
 }
