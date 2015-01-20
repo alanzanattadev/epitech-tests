f19be3879058bc09f0d1fe79d5d3852951c6b765
Kern Matthieu <kern_m@epitech.eu>
2015-01-20 10:55:47 +0100

    Added tests ex02 d14m

---
+++ tek2/piscine/d14m/ex02/main.cpp
@@ -0,0 +1,36 @@
+
+#include "Coconut.h"
+#include "LittleHand.h"
+#include <iostream>
+
+int	main()
+{
+  Coconut**		pack = new Coconut*[14];
+  FruitBox * const *	boxes;
+  int			i = -1;
+
+  boxes = LittleHand::organizeCoconut(NULL);
+  std::cout << boxes << std::endl;
+
+  while (++i < 13)
+    pack[i] = new Coconut;
+  pack[i] = NULL;
+  boxes = LittleHand::organizeCoconut(pack);
+  delete [] pack;
+  i = -1;
+  while (boxes[++i])
+    {
+      std::cout << boxes[i]->nbFruits() << std::endl;
+      delete boxes[i];
+    }
+  delete [] boxes;
+
+  pack = new Coconut*[1];
+  pack[0] = NULL;
+  boxes = LittleHand::organizeCoconut(pack);
+  delete [] pack;
+  std::cout << boxes[0] << std::endl;
+  delete [] boxes;
+
+  return 0;
+}
--- tek2/piscine/d14m/tests.js
+++ tek2/piscine/d14m/tests.js
@@ -5,5 +5,6 @@ console.log("\x1b[36mCOCONUUUUUUUUUUUUUUUUUUUUT !\x1b[0m");
 
 module.exports = testCase({
     'ex00': testCase(tests.getTestCase("Fruitbox",			"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
-    'ex01': testCase(tests.getTestCase("Littlehands (Ya Bon !)",	"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1"))
+    'ex01': testCase(tests.getTestCase("Littlehands (Ya Bon !)",	"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1")),
+    'ex02': testCase(tests.getTestCase("Node.coco",			"/tmp/epitech-tests/test02",		"/tmp/epitech-tests/ex02/outputs/out1"))
 });
