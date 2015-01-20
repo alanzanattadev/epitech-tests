2a2c8319844761ed77f6bfa5d12244088f4e027a
fische_m <fische_m@epitech.eu>
2015-01-20 20:50:11 +0100

    test ex01

---
+++ tek2/piscine/d14a/ex01/BaseComponent.cpp
@@ -0,0 +1,3 @@
+#include "BaseComponent.hpp"
+
+BaseComponent::~BaseComponent() {}
--- tek2/piscine/d14a/tests.js
+++ tek2/piscine/d14a/tests.js
@@ -4,5 +4,6 @@ var tests = require('../../../tests.js');
 console.log("\x1b[36mFische Error: CommumunictionExcption, Gros PD !\x1b[0m");
 
 module.exports = testCase({
-    'ex00': testCase(tests.getTestCase("NasaError",			"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1"))
+    'ex00': testCase(tests.getTestCase("NasaError",			"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
+    'ex01': testCase(tests.getTestCase("UnitTests",			"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out"))
 });
