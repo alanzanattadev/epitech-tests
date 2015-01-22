c98ebf4250dbe417ccc58e132a31639cb3fb9f8d
Kern Matthieu <kern_m@epitech.eu>
2015-01-23 00:33:56 +0100

    Fixed ex03

--- tek2/piscine/d16/ex03/main.cpp
+++ tek2/piscine/d16/ex03/main.cpp
@@ -6,13 +6,12 @@ int main()
 {
   BF_Translator	bf;
 
-  if (bf.translate("lol", "out.c") != 0)
+  if (bf.translate("lol", "/tmp/epitech-tests/out.c") != 0)
     std::cout << "GOOD1" << std::endl;
-  if (bf.translate("in.bf", "NO.c") != 0)
+  if (bf.translate("/tmp/epitech-tests/ex03/in.bf", "/tmp/epitech-tests/NO.c") != 0)
     std::cout << "GOOD2" << std::endl;
-  if (bf.translate("in.bf", "out.c") == 0)
+  if (bf.translate("/tmp/epitech-tests/ex03/in.bf", "/tmp/epitech-tests/out.c") == 0)
     std::cout << "GOOD3" << std::endl;
-  bf.translate("in2.bf", "out2.c");
+  bf.translate("/tmp/epitech-tests/ex03/in2.bf", "/tmp/epitech-tests/out2.c");
   return 0;
 }
-
--- tek2/piscine/d16/tests.js
+++ tek2/piscine/d16/tests.js
@@ -7,6 +7,14 @@ module.exports = testCase({
   'ex00': testCase(tests.getTestCase("eval_expr",		"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
   'ex01': testCase(tests.getTestCase("DomesticKoala",		"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1")),
   'ex02': testCase(tests.getTestCase("EventManager",		"/tmp/epitech-tests/test02",		"/tmp/epitech-tests/ex02/outputs/out1")),
-  'ex03': testCase(tests.getTestCase("BF_Translator",		"/tmp/epitech-tests/test03",		"/tmp/epitech-tests/ex03/outputs/out1")),
+  'ex03': testCase(tests.getTestCase("BF_Translator",
+    "cp /tmp/epitech-tests/ex03/NO.c /tmp/epitech-tests/;" +
+      "chmod 444 /tmp/epitech-tests/NO.c;" +
+      "/tmp/epitech-tests/test03;" +
+      "gcc -W -Wall -Wextra /tmp/epitech-tests/out.c -o /tmp/epitech-tests/out1;" +
+      "/tmp/epitech-tests/out1;" +
+      "gcc -W -Wall -Wextra /tmp/epitech-tests/out2.c -o /tmp/epitech-tests/out2;" +
+      "/tmp/epitech-tests/out2 < /tmp/epitech-tests/ex03/inputs/in1",
+    "/tmp/epitech-tests/ex03/outputs/out1")),
   'ex04': testCase(tests.getTestCase("Ratatard",		"/tmp/epitech-tests/test04",		"/tmp/epitech-tests/ex04/outputs/out1"))
 });
