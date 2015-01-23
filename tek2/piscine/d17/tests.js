fa9db33323f4265a5dece6dafa82e84b0a3697f1
fische_m <fische_m@epitech.eu>
2015-01-23 14:25:14 +0100

    test ex03 in 2 parts

--- tek2/piscine/d17/tests.js
+++ tek2/piscine/d17/tests.js
@@ -8,5 +8,6 @@ module.exports = testCase({
   'ex01': testCase(tests.getTestCase("XXXXXXXXXXXX",		"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1")),
   'ex02-1': testCase(tests.getTestCase("Caesar",		"/tmp/epitech-tests/test02-1 | cat -e",		"/tmp/epitech-tests/ex02/outputs/out1")),
   'ex02-2': testCase(tests.getTestCase("OnePad",		"/tmp/epitech-tests/test02-2 | cat -e",		"/tmp/epitech-tests/ex02/outputs/out2")),
-  'ex03': testCase(tests.getTestCase("Encryption",		"/tmp/epitech-tests/test03",		"/tmp/epitech-tests/ex03/outputs/out1"))
+  'ex03-1': testCase(tests.getTestCase("Encryption",		"/tmp/epitech-tests/test03-1 | cat -e",		"/tmp/epitech-tests/ex03/outputs/out1")),
+  'ex03-2': testCase(tests.getTestCase("Static Encryption",		"/tmp/epitech-tests/test03-2 | cat -e",		"/tmp/epitech-tests/ex03/outputs/out2"))
 });
