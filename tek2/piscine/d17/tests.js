c1e5a2541b5ff52143adbcd9337147d9db0daefe
fische_m <fische_m@epitech.eu>
2015-01-23 13:53:29 +0100

    test ex03

--- tek2/piscine/d17/tests.js
+++ tek2/piscine/d17/tests.js
@@ -7,5 +7,6 @@ module.exports = testCase({
   'ex00': testCase(tests.getTestCase("Find me that",		"/tmp/epitech-tests/test00",		"/tmp/epitech-tests/ex00/outputs/out1")),
   'ex01': testCase(tests.getTestCase("XXXXXXXXXXXX",		"/tmp/epitech-tests/test01",		"/tmp/epitech-tests/ex01/outputs/out1")),
   'ex02-1': testCase(tests.getTestCase("Caesar",		"/tmp/epitech-tests/test02-1 | cat -e",		"/tmp/epitech-tests/ex02/outputs/out1")),
-  'ex02-2': testCase(tests.getTestCase("OnePad",		"/tmp/epitech-tests/test02-2 | cat -e",		"/tmp/epitech-tests/ex02/outputs/out2"))
+  'ex02-2': testCase(tests.getTestCase("OnePad",		"/tmp/epitech-tests/test02-2 | cat -e",		"/tmp/epitech-tests/ex02/outputs/out2")),
+  'ex03': testCase(tests.getTestCase("Encryption",		"/tmp/epitech-tests/test03",		"/tmp/epitech-tests/ex03/outputs/out1"))
 });
